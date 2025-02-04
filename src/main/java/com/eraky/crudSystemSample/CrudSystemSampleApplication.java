a7491a97bce39613a684fc94958435202c31bd81
Abdulrahman Khaled <kabdulrahman115@gmail.com>
2025-02-04 18:31:53 +0200

    first intialization

---
+++ src/main/java/com/eraky/crudSystemSample/CrudSystemSampleApplication.java
@@ -0,0 +1,13 @@
+package com.eraky.crudSystemSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSystemSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSystemSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eraky/crudSystemSample/CrudSystemSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eraky.crudSystemSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSystemSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
